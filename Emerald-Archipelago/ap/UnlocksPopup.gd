1a7b0b8ab275c0b5858928402a1fb0831321259f
J W <22449090+josephwhite@users.noreply.github.com>
2025-12-30 10:46:22 -0500

    more groups, power, and a changelog

--- Emerald-Archipelago/ap/UnlocksPopup.gd
+++ Emerald-Archipelago/ap/UnlocksPopup.gd
@@ -15,6 +15,7 @@ var items: Dictionary = {
 	"Attack Speed": {},
 	"Max Health Increase": {},
 	"Health Regen": {},
+	"Life Steal": {},
 	"SpawnRate Increase": {},
 	"Armor Increase": {},
 	"Infinity": {},
@@ -26,7 +27,8 @@ const item_groups: Dictionary = {
 	"Damage Increase": ["Damage1","DamagePerEnemy1","BossDamage1","Damage2","Damage3","Undamaged1","Execute1","Damage4","BossDamage2","CritDamage1","Damage5","Undamaged2","Execute2","RampingDamage1","CritDamage2","MaxHealthToDamage1"],
 	"Attack Speed": ["AttackSpeed1","AttackSpeed2"],
 	"Max Health Increase": ["Health1","Health2","Health3","Health4","Health5","Health6","Health7"],
-	"Health Regen": ["HealthRegen1","Salvaging1","Lifesteal1","HealthRegen2","Salvaging2","DropHeal1","MaxHealthHeal1","Lifesteal2","Lifesteal3","StealMaxHealth1","MaxHealthHeal2","StealMaxHealth2","StealMaxHealth3"],
+	"Health Regen": ["HealthRegen1","HealthRegen2","DropHeal1","MaxHealthHeal1","StealMaxHealth1","MaxHealthHeal2","StealMaxHealth2","StealMaxHealth3"],
+	"Life Steal": ["Salvaging1","Lifesteal1","Salvaging2","Lifesteal2","Lifesteal3"],
 	"SpawnRate Increase": ["SpawnRate1","SpawnRate2","SpawnRate3","SpawnRate4","NodeFinder1","YellowSpawn1","YellowSpawn2"],
 	"Armor Increase": ["Armor1","BossArmor1","Armor2","ArmorPerEnemy1","Armor3","Armor4","BossArmor2","Armor5","Armor6","MaxHealthToArmor1","Armor7","FocusArmor1","MaxHealthToArmor2","RampingArmor1"],
 	"Infinity": ["Infinity1","Infinity2","Infinity3","Infinity4","Infinity5","Infinity6","Infinity7","Infinity8","Infinity9"],
@@ -57,10 +59,9 @@ func _add_cell_to_table(id:String,curr:int,max:int) -> void:
 
 func _refresh_ui() -> void:
 	body.clear()
-	
 	body.append_text("[center][table=1]")
-	
-	for itemName in apClient._item_id_to_name.values(): # Sorts through every item in the. and figures out how many you have and how many there can be.
+	# Sorts through every item in the datapackage and figures out how many you have and how many there can be.
+	for itemName in apClient._item_id_to_name.values():
 		var curr = 0
 		var max = 0
 		if mainMod.collected_items.has(itemName):
@@ -72,32 +73,15 @@ func _refresh_ui() -> void:
 		elif MilestoneStore.search(itemName) != null:
 			var milestone: Milestone = MilestoneStore.search(itemName)
 			max = 1
-			if mainMod.milestones_in_itempool == false:
-				max = 0
 		match itemName:
 			"CryptoLevel":
 				max = 36
-				if mainMod.crypto_levels_in_itempool == false:
-					max = 0
-				
 			"Boss Drop":
-				match mainMod.bossdrops_setting:
-					0:
-						max = 0
-					1:
-						max = 18
-					2:
-						max = 26
+				max = 26
 			"Extra Bits":
-				if mainMod.bossdrops_setting == 1:
-					max = 4
-				else:
-					max = 0
+				max = 1
 			"Extra Nodes":
-				if mainMod.bossdrops_setting == 1:
-					max = 4
-				else:
-					max = 0
+				max = 1
 		if max == 0:
 			continue
 		_add_cell_to_table(itemName,curr,max)
